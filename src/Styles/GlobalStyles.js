f7ad41a5f50e619ce313d4e5245c7567040921bb
msun4100 <msun4100@gmail.com>
2020-04-13 15:56:49 +0900

    #6.2 Header Logic. (rendering issue; using optional chaining

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -10,8 +10,9 @@ export default createGlobalStyle`
   body {
     background-color:${(props) => props.theme.bgColor};
     color:${(props) => props.theme.blackColor};
-    font-size:14px;
+    font-size: 14px;
     font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding-top: 140px;
   }
   a {
     color:${(props) => props.theme.blueColor};
