9f4a82170c1007166e56098798062cd5c6537654
msun4100 <msun4100@gmail.com>
2020-04-16 18:07:45 +0900

    #9.2 Profile Screen and LogOut

--- src/Components/FollowButton/FollowButtonContainer.js
+++ src/Components/FollowButton/FollowButtonContainer.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import FollowButtonPresenter from "./FollowButtonPresenter";
 import { useMutation } from "react-apollo-hooks";
 import { FOLLOW, UNFOLLOW } from "./FollowButtonQueries";
+import FollowButtonPresenter from "./FollowButtonPresenter";
 
 const FollowButtonContainer = ({ isFollowing, id }) => {
   const [isFollowingS, setIsFollowing] = useState(isFollowing);
   const [followMutation] = useMutation(FOLLOW, { variables: { id } });
   const [unfollowMutation] = useMutation(UNFOLLOW, { variables: { id } });
 
-  const onClick = (e) => {
+  const onClick = () => {
     if (isFollowingS === true) {
       setIsFollowing(false);
       unfollowMutation();
@@ -18,7 +18,6 @@ const FollowButtonContainer = ({ isFollowing, id }) => {
       followMutation();
     }
   };
-
   return <FollowButtonPresenter onClick={onClick} isFollowing={isFollowingS} />;
 };
 
--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
@@ -13,6 +14,9 @@ const Post = styled.div`
   max-width: ${POST_MAX_SIZE};
   margin-bottom: 25px;
   user-select: none;
+  a {
+    color: inherit;
+  }
 `;
 
 const Header = styled.header`
@@ -102,6 +106,10 @@ const Comment = styled.li`
   }
 `;
 
+const Caption = styled.div`
+  margin: 10px 0;
+`;
+
 export default ({
   user: { username, avatar },
   location,
@@ -115,12 +123,15 @@ export default ({
   onKeyPress,
   comments,
   selfComments,
+  caption,
 }) => (
   <Post>
     <Header>
       <Avatar size="sm" url={avatar} />
       <UserColumn>
-        <FatText text={username} />
+        <Link to={`${username}`}>
+          <FatText text={username} />
+        </Link>
         <Location>{location}</Location>
       </UserColumn>
     </Header>
@@ -140,7 +151,10 @@ export default ({
         </Button>
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
-
+      <Caption>
+        <FatText text={username} />
+        {caption}
+      </Caption>
       {comments && (
         <Comments>
           {comments.map((comment) => (
